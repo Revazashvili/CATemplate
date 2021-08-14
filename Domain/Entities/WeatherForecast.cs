9f4b4ffe76c77f134e116a35f606cfba3e41b621
Revazashvili <levanrevazashvili@gmail.com>
2021-08-14 16:36:50 +0400

    gitignore is not working

--- Domain/Entities/WeatherForecast.cs
+++ Domain/Entities/WeatherForecast.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using Domain.Common;
 
 namespace Domain.Entities
 {
-    public class WeatherForecast
+    public class WeatherForecast : Auditable
     {
+        public long Id { get; set; }
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-
         public string Summary { get; set; }
     }
 }
